b60ba829c75b3a9e3b552ca12f6b70269ff34050
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-02-06 19:25:00 -0800

    Fonished CircleRing

--- src/CircleRing.java
+++ src/CircleRing.java
@@ -1,28 +1,41 @@
+import org.jointheleague.graphical.robot.Robot;
+
 /*
  *    Copyright (c) The League of Amazing Programmers 2013-2017
  *    Level 0
  */
 
-import org.jointheleague.graphical.robot.Robot;
+
 
 public class CircleRing {
     public static void main(String[] args) throws Exception {
 
         // 1. Make a new Robot
-
-
+    	Robot C3PO = new Robot();
+    	
        //  2. Set your robot’s position to x=150 and y=200
-
+    	C3PO.setX(150);
+    	C3PO.setY(200);
         // 3. Put the robot's pen down
-
+    	C3PO.penDown();
         // 4. Set the robot’s speed to 10
-
+    	C3PO.setSpeed(100);
         // 5. Do everything below here 360 times (use i as the counter)
-
+    	for (int i = 0; i < 360; i++) {
+			
+		
 	        // 6. Move the robot 3 pixels
-
+    		C3PO.move(3);
+    		C3PO.turn(1);
+    		if (i % 20 == 0) {
+    			for (int x = 0; x < 360; x++) {
+				C3PO.move(1);
+				C3PO.turn(1);
+    			}
+			}
+    		System.out.println(i);
        //  7. Turn the robot 1 degree
-
+    	
         // 8. If the counter i is divisible by 20 (hint: use mod operator  %)
 
 
@@ -31,5 +44,6 @@ public class CircleRing {
 		        // 10. Move the robot 1 pixel
 
 		        // 11. Turn the robot 1 degree
+    	}
 }
 }
--- src/EvilPolygonsChallenge.java
+++ src/EvilPolygonsChallenge.java
@@ -5,23 +5,32 @@ import javax.swing.JOptionPane;
 import org.jointheleague.graphical.robot.Robot;
 
 public class EvilPolygonsChallenge {
-	
+
 	public static void main(String[] args) {
 		//1. Create a robot
-
+		Robot C3P0 = new Robot();
 		//2. Set the speed to 100
-
-		int colorChoice=JOptionPane.showOptionDialog(null, "hello", "title", JOptionPane.INFORMATION_MESSAGE, JOptionPane.DEFAULT_OPTION, null, new String[]{"Color 1","Color 2", "Color 3"}, 0);
+		C3P0.setSpeed(100);
+		int colorChoice=JOptionPane.showOptionDialog(null, "hello", "title", JOptionPane.INFORMATION_MESSAGE, JOptionPane.DEFAULT_OPTION, null, new String[]{"Red","Green", "Blue"}, 0);
 		
 		//3. Set the pen color of the robot according to the value of the colorChoise variable
-		
+		if (colorChoice == 0) {
+			C3P0.setPenColor(255,0,0);
+		} else if (colorChoice == 1) {
+		C3P0.setPenColor(0,255,0);
+		} else if  (colorChoice == 2) {
+			C3P0.setPenColor(0,0,255);
+		} 
 		//4. Ask the use how many polygons they want to be drawn.
-		
+		String num = JOptionPane.showInputDialog("How many polygons u want?");
+		int numb = Integer.parseInt(num);
+		int numbe = 100/numb
+		int size =   
 		//5. Use the robot to draw the number of polygons the user requested.
-		
+		C3P0.move();
 		//6. Make it so your shapes do not overlap
 
 		//7. Challenge: add more colors to the Option Dialog.
 	}
+	
 }
-
